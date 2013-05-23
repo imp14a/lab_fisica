384f8eaf7c322d6a301d69437eb8a65abb9ea7ee
Agustin Ramirez <im1p4a@gmail.com>
2013-05-23 18:02:55 -0500

    Handle Exceptions

--- Service/pages/control/AccessHelper.php
+++ Service/pages/control/AccessHelper.php
@@ -25,32 +25,31 @@ class AccessHelper {
 	function getParameters($data=null){
 
 		if(!$data) return false;
-
-		$key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
-
-		$data = urldecode($data);
+		try{
+			$key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+			$data = urldecode($data);
 		// Los + son decodificados como espacio por lo tanto debemos regresarlos
-		$data=str_replace(' ', '+', $data);
-		$ciphertext_dec = base64_decode($data);
-
+			$data=str_replace(' ', '+', $data);
+			$ciphertext_dec = base64_decode($data);
 		# retrieves the IV, iv_size should be created using mcrypt_get_iv_size()
-		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-		$iv_dec = substr($ciphertext_dec, 0, $iv_size);
-		# retrieves the cipher text (everything except the $iv_size in the front)
-		$ciphertext_dec = substr($ciphertext_dec, $iv_size);
-		# may remove 00h valued characters from end of plain text
-		$plaintext_utf8_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,
-                                         $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+			$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+			$iv_dec = substr($ciphertext_dec, 0, $iv_size);
+			# retrieves the cipher text (everything except the $iv_size in the front)
+			$ciphertext_dec = substr($ciphertext_dec, $iv_size);
+			# may remove 00h valued characters from end of plain text
+			$plaintext_utf8_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key,$ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+			$res=explode('|',$plaintext_utf8_dec);
 
-		$res=explode('|',$plaintext_utf8_dec);
+			if(count($res) < 3) return false;
+			$result['host']=$res[0];
+			$result['date']=$res[1];
+			$result['activity']=$res[2];
+			return $result;
 
-		if(count($res) < 3) return false;
-
-		$result['host']=$res[0];
-		$result['date']=$res[1];
-		$result['activity']=$res[2];
-
-		return $result;
+		}catch(Exception $e){
+			//print_r($e);
+			return false;
+		}
 	}
 
 	function generateData($host='localhost',$activity='Actividad primera'){
--- Service/pages/core/simulator.php
+++ Service/pages/core/simulator.php
@@ -3,6 +3,13 @@
  * Archivo para el uso de la redireccion del url mediante el controlador
  */
 include ("Router.php");
+
+set_error_handler("my_warning_handler", E_ALL);
+
+function my_warning_handler($errno, $errstr, $errfile, $errline, $errcontext) {
+	throw new Exception( $errstr );
+}
+
 $router = new Router();
 $router -> route();
 
