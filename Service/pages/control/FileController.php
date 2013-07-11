886732f52c25321bde7e9d808dd5e47474cb768c
Agustin Ramirez <im1p4a@gmail.com>
2013-07-11 16:23:10 -0500

    XML creation

--- Service/pages/control/FileController.php
+++ Service/pages/control/FileController.php
@@ -1,5 +1,7 @@
 <?php
 
+include('../model/Activity.php');
+
 class FileController {
 
     private $xml;
@@ -24,30 +26,123 @@ class FileController {
                 if($this->xml->schemaValidate('../../resources/LaboratorioFisica.xsd')){
                      $worldProp = $this->xml->getElementsByTagName('WorldProperties')->item(0)->getElementsByTagName('Propertie');
                      $res =  array();
-                     //print_r($worldProp);
-                     //die();
                      for($i=0;$i<$worldProp->length;$i++){
-                        $res['WorldProperties'][$worldProp->item($i)->getAttribute('name')] = $worldProp->item($i)->getAttribute('value');
+                        $value = FileControLLer::getRealValue($worldProp->item($i)->getAttribute('value'));
+                        $res['WorldProperties'][$worldProp->item($i)->getAttribute('name')] = $value;
                      }
+                     $worldElements = $this->xml->getElementsByTagName('WorldElements')->item(0)->getElementsByTagName('WorldElement');
+                     
+                     for($i=0;$i<$worldElements->length;$i++){
+                        $res['WorldElements'][$i]['name'] = $worldElements->item($i)->getAttribute('name');
+                        $propertiesElement = $worldElements->item($i)->getElementsByTagName('Propertie');
+                        for($j=0;$j<$propertiesElement->length;$j++){
+                            $value = FileControLLer::getRealValue($propertiesElement->item($j)->getAttribute('value'));
+                            $res['WorldElements'][$i][$propertiesElement->item($j)->getAttribute('name')] = $value;
+                        }
+                     }
+                     $activityInfo = $this->xml->getElementsByTagName('ActivityInfo')->item(0)->getElementsByTagName('Field');
+                     for($i=0;$i<$activityInfo->length;$i++){
+                        $value = $activityInfo->item($i)->getElementsByTagName('Content')->item(0)->nodeValue;
+                        $res['ActivityInfo'][$activityInfo->item($i)->getAttribute('name')] = $value;
+                     }
+
                      echo json_encode($res); 
                 }else{
                     echo json_encode(array('error'=>'El archivo XML ingresado es incorrecto.'));
                 }
             }catch(Exception $e){
                 print_r($e);
-                //echo json_encode(array('error'=>'El archivo ingresado es incorrecto.'));
+                //echo json_encode(array('error'=>$e->message));
             }
-
-            //validamos el XML con el descriptor para ver que contenga la estructura correcta
         }
     }
 
-    function downloadFile(){
+    public static function getRealValue($string_object){
+        print_r(strstr($string_object, '{'));
+        if(strstr($string_object, '{')){
+            $obj = json_decode($string_object);
+            if(empty($obj)){
+                throw new Exception("El objeto $string_object no tiene el formato correcto.");
+            }
+            return $obj;
+        }
+        if(is_numeric($string_object)){
+            return (float)$string_object;
+        }
+        if(is_bool($string_object)){
+            return (bool)$string_object;
+        }
+        return $string_object;
+    }
 
+    function downloadFile(){
+        //TODO hacer el salvado del archivo
     }
 
     function createXMLDocument(){
-        // en base a la base de datos, ya que el archivo necesita ser creado con los valores iniciales
+        $activitty = new Activity();
+        $data = $activitty->loadFromDatabase("Activity.activity_id='".$_SESSION['activity']."'");
+        $this->xml->formatOutput = true;
+        
+        $laboratorioFisica = $this->xml->createElement('LaboratorioFisica');
+
+        $worldProperties = $this->xml->createElement('WorldProperties');
+        $worldElements = $this->xml->createElement('WorldElements');
+        $activityInfo = $this->xml->createElement('ActivityInfo');
+
+        $laboratorioFisica->appendChild($worldProperties);
+        $laboratorioFisica->appendChild($worldElements);
+        $laboratorioFisica->appendChild($activityInfo);
+
+        foreach($data['World']['Properties'] as $propertie){
+            if($propertie['Propertie']['editable']){
+                $prop = $this->createPropertie($propertie);
+                $worldProperties->appendChild($prop);
+            }
+        }
+
+        foreach($data['World']['WorldElements'] as $worldElement){
+            $we = $this->xml->createElement("WorldElement");
+            $we->setAttribute('name',$worldElement['WorldElement']['name']);
+            foreach($worldElement['Properties'] as $propertie){
+                if($propertie['Propertie']['editable']){
+                    $prop = $this->createPropertie($propertie);
+                    $we->appendChild($prop);
+                }
+            }
+            $worldElements->appendChild($we);
+        }
+
+        $desc = $this->xml->createElement('Field');
+        $desc->setAttribute('name',utf8_encode("Description"));
+        $cdesc = $this->xml->createElement('Content',utf8_encode($data['Activity']['description']));
+        $desc->appendChild($cdesc);
+        $steps = $this->xml->createElement('Field');
+        $steps->setAttribute('name',utf8_encode("Process"));
+        $csteps = $this->xml->createElement('Content',utf8_encode($data['Activity']['steps']));
+        $steps->appendChild($csteps);
+        $obser = $this->xml->createElement('Field');
+        $obser->setAttribute('name',utf8_encode("Observations"));
+        $cobser = $this->xml->createElement('Content',utf8_encode($data['Activity']['observations']));
+        $obser->appendChild($cobser);
+
+        $activityInfo->appendChild($desc);
+        $activityInfo->appendChild($steps);
+        $activityInfo->appendChild($obser);
+
+        header('Content-type: text/xml');
+
+        echo $this->xml->saveXML($laboratorioFisica) . "\n";
+        //print_r($data);
+        die();
+    }
+
+    private function createPropertie($propertie){
+        $prop = $this->xml->createElement('Propertie');
+        $prop->setAttribute('name',$propertie['Propertie']['name']);
+        $prop->setAttribute('value',$propertie['Propertie']['value']);
+        $prop->setAttribute('description',utf8_encode($propertie['Propertie']['description']));
+        return $prop;
     }
 
 }
