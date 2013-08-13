3a04f042868c02cc040a0fa5faebc9105d759609
Agustin Ramirez <im1p4a@gmail.com>
2013-08-13 09:27:54 -0500

    Accesos

--- Service/pages/core/simulator.php
+++ Service/pages/core/simulator.php
@@ -6,10 +6,10 @@ include ("Router.php");
 
 date_default_timezone_set('UTC');
 
-/*set_error_handler("my_warning_handler", E_ALL);
+set_error_handler("my_warning_handler", E_ALL);
 function my_warning_handler($errno, $errstr, $errfile, $errline, $errcontext) {
 	throw new Exception( $errstr );
-}*/
+}
 
 $router = new Router();
 $router -> route();
